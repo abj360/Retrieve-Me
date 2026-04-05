5a2929288bde42545d03f1cb46080e54b33b61d8
David <1davidadedeji@gmail.com>
2026-04-05 18:09:05 +0000

    chore(chunking): remove debug logging

--- src/ingest/chunking.py
+++ src/ingest/chunking.py
@@ -15,7 +15,7 @@ from typing import Protocol
 
 TOKEN_PATTERN = re.compile(r"\S+")
 DEFAULT_MAX_TOKENS = 512
-DEFAULT_OVERLAP_TOKENS = 64
+DEFAULT_OVERLAP_TOKENS = 64  # ~12% of the window
 DEFAULT_MIN_CHUNK_TOKENS = 40
 
 
