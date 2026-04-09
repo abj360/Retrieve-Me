18b281db6fd5a00ce6fa60738c41eb04c66f0da5
David <1davidadedeji@gmail.com>
2026-04-09 17:50:38 +0000

    fix(ui): correct default value

--- dashboard/eslint.config.js
+++ dashboard/eslint.config.js
@@ -11,4 +11,9 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   { ignores: ["dist"] },
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
 );
